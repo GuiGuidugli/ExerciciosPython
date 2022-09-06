963d8073afeb840e60f44f716f497209630865b4
GuiGuidugli <105385009+GuiGuidugli@users.noreply.github.com>
2022-09-06 15:41:24 -0300

    Código em aberto

--- Loja de tintas next level.py
+++ Loja de tintas next level.py
@@ -1,11 +1,23 @@
 import math
-area= float(input('Insira o tamanho da área a ser pintada em metros quadrados: '))
-litros = area/6
-latas = litros/18
-galoes = litros/3.6
-latas_arredondada = math.ceil(latas)
-galoes_arredondado = math.ceil(galoes)
-preco_latas = latas_arredondada*80
-preco_galoes = galoes_arredondado*25
-print(f'A quantidade de litros a ser comprada é {litros:.1f}. Caso você queira comprar apenas latas, será necessário comprar {latas_arredondada} lata(s), pelo preço de {preco_latas} reais. Caso você queira comprar apenas galões, será necessário comprar {galoes_arredondado} galão(ões), pelo preço de {preco_galoes} reais. Caso você queria misturar ambos, será necessário comprar ')
-#Falta o código pra misturar#
+
+Area = int(input("Qual a medida da area a ser pintada em metros quadrados? "))
+AreaStr = str(Area)
+newArea = Area * 1.1 #Esta fórmula define 10% de folga para a área recebida#
+precoLata = 80
+precoGalao = 25
+coberturaLata = 18*6
+coberturaGalao = 3.6*6 #Este e o de cima mostram a área que cada recipiente cobre#
+
+if (newArea <= coberturaLata): print (f'Será necessário apenas uma lata de tinta, ao preço de {precoLata} reais.') #Define apenas uma lata se a área coberta requisitar apenas uma lata#
+else:
+     latasNecessarias = newArea // coberturaLata #Define a qtde de latas necessárias para cobrir a área recebida#
+     galoesNecessarios = (newArea - (latasNecessarias * 108)) / 21.6 #Define a qtde de galões necessários depois de compensado o quanto foi coberto pelas latas#
+     if (galoesNecessarios % 1 == 0):
+         galoesNecessarios = galoesNecessarios
+     else: (galoesNecessarios // 1) + 1  
+
+totalLatas = latasNecessarias * precoLata
+totalGaloes = galoesNecessarios * precoGalao
+total = totalLatas + totalGaloes
+
+print (f'Serão necessárias {latasNecessarias} lata(s) de tinta e {math.ceil(galoesNecessarios)} galão(ões) de tinta para cobrir a área a ser pintada; e o preço total é {total:.1f}')
---
+++ teste.py
@@ -0,0 +1,6 @@
+Variavel = float(input('Insira um valor: '))
+newVariavel = (f'{Variavel:.2f}')
+
+print (newVariavel)
+
+
