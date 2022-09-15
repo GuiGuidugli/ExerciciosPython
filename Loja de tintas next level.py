cf1c41411c9f1de774d76194938abeba33b97cf9
Guilherme Guidugli <105385009+GuiGuidugli@users.noreply.github.com>
2022-09-14 21:55:49 -0300

    Update Loja de tintas next level.py

--- Loja de tintas next level.py
+++ Loja de tintas next level.py
@@ -8,4 +8,3 @@ galoes_arredondado = math.ceil(galoes)
 preco_latas = latas_arredondada*80
 preco_galoes = galoes_arredondado*25
 print(f'A quantidade de litros a ser comprada é {litros:.1f}. Caso você queira comprar apenas latas, será necessário comprar {latas_arredondada} lata(s), pelo preço de {preco_latas} reais. Caso você queira comprar apenas galões, será necessário comprar {galoes_arredondado} galão(ões), pelo preço de {preco_galoes} reais. Caso você queria misturar ambos, será necessário comprar ')
-#Falta o código pra misturar#
