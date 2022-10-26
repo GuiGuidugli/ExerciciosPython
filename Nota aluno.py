99d9bbc929227f0bfc41b2248be7e35b943adfaf
GuiGuidugli <105385009+GuiGuidugli@users.noreply.github.com>
2022-10-25 21:46:03 -0300

    Exercicio 8

--- Nota aluno.py
+++ Nota aluno.py
@@ -1,6 +1,8 @@
 nota1 = float(input("Digite a primeira nota do aluno:\n\r "))
-nota2 = float (input("Digite a segunda nota do aluno:\n\r "))
+nota2 = float(input("Digite a segunda nota do aluno:\n\r "))
+
 media = (nota1 + nota2) / 2
+
 if media >= 7 and media < 10:
     print ("Aluno aprovado!")
 elif media == 10:
---
+++ exercicio8.py
@@ -0,0 +1,35 @@
+estoque = []
+choice = 0
+codigo = 1
+while choice != 4:
+    choice = int(input("Selecione sua opção:\n1 - Adicionar Livro\n2 - Consultra Livro\n3 - Inventario\n4 - Sair\n=> "))
+    if choice == 1:
+        autor = input("Qual o nome do autor: ")
+        title = input("Qual o nome do livro: ")
+        editor = input("Qual a editora: ")
+        year = int(input("Qual o ano de lançamadawdadwadwadaento: "))
+        quantidade = int(input("Quantidade de loibros: "))
+        valor = float(input("Valor do livri: "))
+        estoque.append({
+            "registro": codigo,
+            "autor": autor,
+            "titulo": title,
+            "editora": editor,
+            "ano": year,
+            "quantidade": quantidade,
+            "valor": valor
+            })
+        codigo = codigo + 1
+    elif choice == 2:
+        codigoDoItem = int(input("Digite o codigo do livroa: "))
+        encontrado = False
+        for item in estoque:
+            if item["registro"] == codigoDoItem:
+                encontrado = True
+                break
+        if encontrado == True:
+            print(item)
+        else:
+            print("Livro não encontrado")
+    elif choice == 3:
+        print(estoque)
